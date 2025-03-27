0674fb78f26f1e3a9884f5caacce933a4feaa44c
Raja Sekhar <rajshekhar.k321@gmail.com>
2025-03-27 13:52:54 +0530

    fix: redux changes

--- src/containers/custom-procedures.jsx
+++ src/containers/custom-procedures.jsx
@@ -23,13 +23,11 @@ class CustomProcedures extends React.Component {
       warp: false,
     }
   }
-
   componentWillUnmount() {
     if (this.workspace) {
       this.workspace.dispose()
     }
   }
-
   setBlocks(blocksRef) {
     if (!blocksRef) return
     this.blocks = blocksRef
@@ -37,54 +35,81 @@ class CustomProcedures extends React.Component {
       rtl: this.props.isRtl,
     })
 
+    // @todo This is a hack to make there be no toolbox.
     const oldDefaultToolbox = ScratchBlocks.Blocks.defaultToolbox
     ScratchBlocks.Blocks.defaultToolbox = null
     this.workspace = ScratchBlocks.inject(this.blocks, workspaceConfig)
     ScratchBlocks.Blocks.defaultToolbox = oldDefaultToolbox
 
+    // Create the procedure declaration block for editing the mutation.
     this.mutationRoot = this.workspace.newBlock('procedures_declaration')
+    // Make the declaration immovable, undeletable and have no context menu
     this.mutationRoot.setMovable(false)
     this.mutationRoot.setDeletable(false)
     this.mutationRoot.contextMenu = false
 
     this.workspace.addChangeListener(() => {
       this.mutationRoot.onChangeFn()
+      // Keep the block centered on the workspace
       const metrics = this.workspace.getMetrics()
       const { x, y } = this.mutationRoot.getRelativeToSurfaceXY()
       const dy = metrics.viewHeight / 2 - this.mutationRoot.height / 2 - y
       let dx
-
       if (this.props.isRtl) {
+        // // TODO: https://github.com/LLK/scratch-gui/issues/2838
+        // This is temporary until we can figure out what's going on width
+        // block positioning on the workspace for RTL.
+        // Workspace is always origin top-left, with x increasing to the right
+        // Calculate initial starting offset and save it, every other move
+        // has to take the original offset into account.
+        // Calculate a new left postion based on new width
+        // Convert current x position into LTR (mirror) x position (uses original offset)
+        // Use the difference between ltrX and mirrorX as the amount to move
         const ltrX = metrics.viewWidth / 2 - this.mutationRoot.width / 2 + 25
         const mirrorX = x - (x - this.state.rtlOffset) * 2
-        if (mirrorX !== ltrX) {
-          dx = mirrorX - ltrX
+        if (mirrorX === ltrX) {
+          return
+        }
+        dx = mirrorX - ltrX
+        const midPoint = metrics.viewWidth / 2
+        if (x === 0) {
+          // if it's the first time positioning, it should always move right
+          if (this.mutationRoot.width < midPoint) {
+            dx = ltrX
+          } else if (this.mutationRoot.width < metrics.viewWidth) {
+            dx = midPoint - (metrics.viewWidth - this.mutationRoot.width) / 2
+          } else {
+            dx = midPoint + (this.mutationRoot.width - metrics.viewWidth)
+          }
           this.mutationRoot.moveBy(dx, dy)
           this.setState({ rtlOffset: this.mutationRoot.getRelativeToSurfaceXY().x })
+          return
+        }
+        if (this.mutationRoot.width > metrics.viewWidth) {
+          dx = dx + this.mutationRoot.width - metrics.viewWidth
         }
       } else {
         dx = metrics.viewWidth / 2 - this.mutationRoot.width / 2 - x
+        // If the procedure declaration is wider than the view width,
+        // keep the right-hand side of the procedure in view.
         if (this.mutationRoot.width > metrics.viewWidth) {
           dx = metrics.viewWidth - this.mutationRoot.width - x
         }
-        this.mutationRoot.moveBy(dx, dy)
       }
+      this.mutationRoot.moveBy(dx, dy)
     })
-
     this.mutationRoot.domToMutation(this.props.mutator)
     this.mutationRoot.initSvg()
     this.mutationRoot.render()
     this.setState({ warp: this.mutationRoot.getWarp() })
-
+    // Allow the initial events to run to position this block, then focus.
     setTimeout(() => {
       this.mutationRoot.focusLastEditor_()
     })
   }
-
   handleCancel() {
     this.props.onRequestClose()
   }
-
   handleOk() {
     const newMutation = this.mutationRoot ? this.mutationRoot.mutationToDom(true) : null
 
@@ -94,25 +119,21 @@ class CustomProcedures extends React.Component {
 
     this.props.onRequestClose()
   }
-
   handleAddLabel() {
     if (this.mutationRoot) {
       this.mutationRoot.addLabelExternal()
     }
   }
-
   handleAddBoolean() {
     if (this.mutationRoot) {
       this.mutationRoot.addBooleanExternal()
     }
   }
-
   handleAddTextNumber() {
     if (this.mutationRoot) {
       this.mutationRoot.addStringNumberExternal()
     }
   }
-
   handleToggleWarp() {
     if (this.mutationRoot) {
       const newWarp = !this.mutationRoot.getWarp()
@@ -120,7 +141,6 @@ class CustomProcedures extends React.Component {
       this.setState({ warp: newWarp })
     }
   }
-
   render() {
     return (
       <CustomProceduresComponent
@@ -141,7 +161,6 @@ CustomProcedures.propTypes = {
   isRtl: PropTypes.bool,
   mutator: PropTypes.instanceOf(Element),
   onRequestClose: PropTypes.func.isRequired,
-  mutatorCallback: PropTypes.func, 
   options: PropTypes.shape({
     media: PropTypes.string,
     zoom: PropTypes.shape({
@@ -152,6 +171,17 @@ CustomProcedures.propTypes = {
     comments: PropTypes.bool,
     collapse: PropTypes.bool,
   }),
+}
+
+CustomProcedures.defaultOptions = {
+  zoom: {
+    controls: false,
+    wheel: false,
+    startScale: 0.9,
+  },
+  comments: false,
+  collapse: false,
+  scrollbars: true,
 }
 
 CustomProcedures.defaultProps = {
