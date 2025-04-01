de95fdf4d9c86453aafb58f269c2d4959e3411f4
Rbel12b <rbel12b@gmail.com>
2025-04-01 20:12:09 +0200

    feat: extension loader, move samlabs to other repo

---
+++ src/lib/libraries/extensions/extensionLoader/index.jsx
@@ -0,0 +1,37 @@
+import React from 'react';
+import {FormattedMessage} from 'react-intl';
+
+import iconImage from './extensionLoader.png';
+import insetImage from './extensionLoader-small.svg';
+
+const translationMap = {
+    'en': {
+        'gui.extension.extensionLoader.name': 'Extension Loader',
+        'gui.extension.extensionLoader.description': 'Load extension from internet.'
+    }
+};
+
+export default {
+    name: (
+        <FormattedMessage
+            defaultMessage="Extension Loader"
+            description="Name for the extension loader selector"
+            id="gui.extension.extensionLoader.name"
+        />
+    ),
+    iconURL: iconImage,
+    insetIconURL: insetImage,
+    description: (
+        <FormattedMessage
+            defaultMessage="Load extension from internet."
+            description="Description for the extension loader selector"
+            id="gui.extension.extensionLoader.description"
+        />
+    ),
+    featured: true,
+    disabled: false,
+    internetConnectionRequired: true,
+    collaborator: 'Yengawa Lab',
+    helpLink: 'https://xcratch.github.io/',
+    translationMap: translationMap
+};
